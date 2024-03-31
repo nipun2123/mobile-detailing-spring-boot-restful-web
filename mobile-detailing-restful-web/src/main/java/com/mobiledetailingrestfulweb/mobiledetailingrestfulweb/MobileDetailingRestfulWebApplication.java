2f39af1ad5358746dd8f5b4085fa1c32e15a26bb
nipun <nipun.jayasanka10@gmail.com>
2024-03-31 17:13:41 +0100

    MDWA-7 Project setup

---
+++ mobile-detailing-restful-web/src/main/java/com/mobiledetailingrestfulweb/mobiledetailingrestfulweb/MobileDetailingRestfulWebApplication.java
@@ -0,0 +1,13 @@
+package com.mobiledetailingrestfulweb.mobiledetailingrestfulweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileDetailingRestfulWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileDetailingRestfulWebApplication.class, args);
+	}
+
+}
---
+++ mobile-detailing-restful-web/src/test/java/com/mobiledetailingrestfulweb/mobiledetailingrestfulweb/MobileDetailingRestfulWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobiledetailingrestfulweb.mobiledetailingrestfulweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileDetailingRestfulWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
